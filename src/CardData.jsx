97f3fb2a2e42e0cd7932f765ce2d5d10a88bb084
Viraj kulkarni <95925653+virumons@users.noreply.github.com>
2023-11-23 18:13:53 +0530

    Update CardData.jsx

--- src/CardData.jsx
+++ src/CardData.jsx
@@ -8,8 +8,8 @@ const CardData = ({Data}) => {
     
           <div className='flex flex-row justify-center items-center '>
             <div className='flex flex-col justify-center items-center mx-[10px] hover:drop-shadow-xl'>
-              <img src={Data.img} className='w-[500px] h-[500px] rounded-tl-3xl rounded-tr-3xl' />
-              <div className='py-[2rem] rounded-br-3xl rounded-bl-3xl bg-[#f4f4f4] w-[500px] flex flex-col justify-center items-center '>
+              <img src={Data.img} className='w-[350px] h-[350px] sm:w-[450px] sm:h-[450px] rounded-tl-3xl rounded-tr-3xl' />
+              <div className='py-[2rem] rounded-br-3xl rounded-bl-3xl bg-[#f4f4f4] w-[350px] sm:w-[450px] flex flex-col justify-center items-center '>
                 <h1 className='text-[20px] font-normal'>Nike Sports collections</h1>
                 <h1 className='text-[50px] font-semibold text-[#2d2d2d]'>{Data.name}</h1>
                 <button className=' py-2 text-[#4971c9] text-[20px] font-medium border-b-[3px] border-[#4971c9] hover:text-black transition duration-100 ease-in-out'>Shop Now</button>
